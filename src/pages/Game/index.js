07ac2e8e8cf94984dabd97c5ca5c7e6af6e61060
darkn8 <imtiaz@envriya.in>
2021-05-26 18:22:20 +0530

    Implement get winner algorithm

--- src/pages/Game/index.js
+++ src/pages/Game/index.js
@@ -1,18 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Board from "../../components/Board";
-import { updateCell } from "./game.helpers";
+import { updateCell, getWinner } from "./game.helpers";
+import { toast } from "react-toastify";
 import "./game.css";
 
-const DEFAULT_ROWS = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
 export default function Game() {
-  const [rows, setRows] = useState(DEFAULT_ROWS);
+  const [rows, setRows] = useState([
+    [null, null, null],
+    [null, null, null],
+    [null, null, null],
+  ]);
   const [isPlayerOne, setIsPlayerOne] = useState(true);
   const [moves, setMoves] = useState(0);
+  const [finished, setFinished] = useState(false);
+
+  useEffect(() => {
+    // If moves is greater or equal to 5 run winner algo
+    const winner = getWinner(rows);
+    console.log("ROW");
+    console.log("called");
+
+    if (winner) {
+      toast.error(`${winner} has won!`, { position: "top-center" });
+      setFinished(true);
+    }
+  }, [rows]);
+
+  useEffect(() => {
+    if (moves >= 9) {
+      const winner = getWinner(rows);
+      setFinished(true);
+      if (!winner) {
+        toast.error("Its a draw!", { position: "top-center" });
+      }
+    }
+  }, [moves]);
+
+  useEffect(() => {
+    if (finished) setMoves(0);
+  }, [finished]);
+
+  const resetBoard = () => {
+    setRows([
+      [null, null, null],
+      [null, null, null],
+      [null, null, null],
+    ]);
+    setIsPlayerOne(true);
+    setFinished(false);
+  };
 
   const handleCellClick = (x, y) => {
     // return if the cell has already been marked
@@ -26,7 +62,20 @@ export default function Game() {
 
   return (
     <div className="game">
-      <h1>{`${isPlayerOne ? "X" : "O"}'s turn`}</h1>
+      <div>
+        {finished ? (
+          <button
+            className="btn-play-again"
+            onClick={(e) => {
+              resetBoard();
+            }}
+          >
+            Play again
+          </button>
+        ) : (
+          <h1>{`${isPlayerOne ? "X" : "O"}'s turn`}</h1>
+        )}
+      </div>
       <Board rows={rows} handleCellClick={handleCellClick} />
     </div>
   );
