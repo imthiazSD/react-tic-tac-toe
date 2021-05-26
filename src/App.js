668a883ac17cfbe053438614b9e142bdbfa6f8be
darkn8 <imtiaz@envriya.in>
2021-05-26 18:20:43 +0530

    Integrate toastify

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from "./logo.svg";
 import Game from "./pages/Game";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Game />
+      <ToastContainer />
     </div>
   );
 }
